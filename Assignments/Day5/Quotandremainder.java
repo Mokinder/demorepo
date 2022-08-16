2b148184a24fbe9ce997ab989a16b8033a9f9704
Mokinder <83770793+Mokinder@users.noreply.github.com>
2022-08-17 00:05:17 +0530

    Add files via upload

---
+++ Assignments/Day5/Quotandremainder.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Quotandremainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int q,r;
+		q=44/5;
+		r=44%5;
+		System.out.println("when we divide 44 by 5 Quotient is "+q+" and Remainder is "+r);
+        
+	}
+
+}
