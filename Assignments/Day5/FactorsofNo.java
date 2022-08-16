868d5a95ede34eca89e1ebefa64b9f6e28bad6cd
Mokinder <83770793+Mokinder@users.noreply.github.com>
2022-08-17 00:14:38 +0530

    Update FactorsofNo.java

--- Assignments/Day5/FactorsofNo.java
+++ Assignments/Day5/FactorsofNo.java
@@ -6,13 +6,15 @@ public class FactorsofNo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n;
+	      int n;
 	      Scanner s = new Scanner(System.in);
 	      System.out.println("Enter a number :");
 	      n = s.nextInt();
 	     
-	      for(int i = 2; i< n; i++) {
-	         while(n%i == 0) {
+	      for(int i = 2; i< n; i++) 
+	      {
+	         while(n%i == 0) 
+		 {
 	            System.out.println(i+" ");
 	            n = n/i;
 	         }
