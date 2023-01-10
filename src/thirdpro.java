0f62b5c82d6dd670d6095edef42d21a2cd8da688
Mokinder <mokindershekaar2006@gmail.com>
2023-01-10 16:41:22 +0530

    third commit changes..

--- src/thirdpro.java
+++ src/thirdpro.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class thirdpro {
+import java.util.*;
+public class thirdpro {
+    public static void main(String args[]){
+        int a[] = new int[10];
+        System.out.println("Enter the numbers : ");
+        Scanner s=new Scanner(System.in);
+        for(int i=0;i<a.length-1;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<a.length-1;i++){
+            System.out.println(a[i]);
+        }
+    }
 }
