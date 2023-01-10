ff3ec2d16d8efe473c6ded3fe79c3ed051d662e5
Mokinder <mokindershekaar2006@gmail.com>
2023-01-10 15:02:05 +0530

    second commit

--- src/firstpro.java
+++ src/firstpro.java
@@ -1,5 +1,7 @@
 public class firstpro {
     public static void main(String args[]){
     System.out.println("hello");
+    second s= new second();
+    s.add();
 }
 }
---
+++ src/second.java
@@ -0,0 +1,8 @@
+public class second extends firstpro {
+    public void add(){
+        int a=4;
+        int b=7;
+        System.out.println("Addition is : "+(a+b));
+    }
+
+}
